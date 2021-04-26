7758b6f0b7ddeb661db45db8c83a3b2469dddabe
avellar1975 <evandro.avellar@gmail.com>
2021-04-26 07:50:57 -0300

    print melhorado do hash

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ if __name__ == '__main__':
     blockchain.add_new_block('$4.5 -> João -> Maria')
     blockchain.add_new_block('$2.5 -> João -> Luiz')
 
-    print(blockchain.get_all())
+    for hash in blockchain.get_all():
+        print(hash)
