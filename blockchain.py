f74a355f0977c12a6cbe8c0286e4eebf029c7b2f
evandro.avellaavell <>
2021-04-27 18:21:56 -0300

    melhorias

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,6 @@
 from hashlib import sha256
 from datetime import datetime
+from time import time
 
 
 class Blockchain:
@@ -9,24 +10,28 @@ class Blockchain:
         self.set_genesis_block()
 
     def set_genesis_block(self):
-        data = '$1000 -> Zero -> --'
+        transaction = '$1000\t De: Satoshi\t -> --\t'
         timestamp = datetime.utcnow().timestamp()
         prev_hash = '000000'
         index = 0
         self.hash_block(
-            data, timestamp, prev_hash, index
+            transaction, timestamp, prev_hash, index
         )
 
-    def hash_block(self, data, timestamp, prev_hash, index):
+    def hash_block(self, transaction, timestamp, prev_hash, index):
         hash = ''
         nonce = 1
+        start_time = time()
         while not self.is_hash_valid(hash):
             block = '{}:{}:{}:{}:{}'.format(
-                data, timestamp, prev_hash, index, nonce
+                transaction, timestamp, prev_hash, index, nonce
             )
             hash = sha256(block.encode()).hexdigest()
             nonce += 1
-        print(index, '[nonce]', nonce, " - ", data, prev_hash[-6:], hash[-6:])
+        print(f'#{index} - [nonce] - {nonce}\t  - {transaction}\t {prev_hash[-6:]}\t {hash[-6:]}')
+        end_time = time()
+        duracao = end_time - start_time
+        print(f'Tempo de mineração:{duracao}\n')
         self.blocks.append(hash)
 
     def get_last_hash(self):
@@ -35,12 +40,12 @@ class Blockchain:
     def is_hash_valid(self, hash):
         return hash.startswith('0000')
 
-    def add_new_block(self, data):
+    def add_new_block(self, transaction):
         index = len(self.blocks)
         prev_hash = self.get_last_hash()
         timestamp = datetime.utcnow().timestamp()
         self.hash_block(
-            data, timestamp, prev_hash, index
+            transaction, timestamp, prev_hash, index
         )
 
     def get_all(self):
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from blockchain import Blockchain
 
-
 if __name__ == '__main__':
     blockchain = Blockchain()
+    blockchain.add_new_block('$3.7\t De: Satochi\t -> Maria')
+    blockchain.add_new_block('$4.5\t De: Joao\t -> Maria')
+    blockchain.add_new_block('$2.6\t De: Joao\t -> Luiz')
 
-    blockchain.add_new_block('$10.2 -> Zero -> João')
-    blockchain.add_new_block('$4.5 -> João -> Maria')
-    blockchain.add_new_block('$2.5 -> João -> Luiz')
+
+    indice = 1
 
     for hash in blockchain.get_all():
-        print(hash)
+        print('\n--------------------------------------------')
+        print(f'#{indice} {hash}')
+        indice += 1
